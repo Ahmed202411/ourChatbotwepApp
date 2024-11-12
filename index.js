f5d30e5eef44961ac984bb2aeb0fa2a20a1c5f19
Ahmed202411 <ahmedlabghial@gmail.com>
2024-11-12 14:33:20 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// استيراد المكتبات المطلوبة
+const express = require('express');      // مكتبة Express لبناء تطبيقات الويب بسهولة
+const bodyParser = require('body-parser'); // مكتبة body-parser لتحليل محتوى الطلبات
+const request = require('request');       // مكتبة request لإرسال طلبات HTTP
+
+// إنشاء تطبيق Express
+const app = express();
+
+// إعداد المنفذ الذي سيستمع عليه التطبيق
+app.set("port", (process.env.PORT || 8000)); // استخدام المنفذ من المتغيرات البيئية أو المنفذ 8000 افتراضياً
+
+// إعدادات body-parser لتحليل محتوى الطلبات
+app.use(bodyParser.urlencoded({ extended: false })); // تحليل محتوى البيانات بشكل URL-encoded
+app.use(bodyParser.json()); // تحليل محتوى البيانات بصيغة JSON
+
+// إنشاء مسار GET عند الوصول إلى "/" وإرسال رسالة بسيطة
+app.get("/", function(req, res){
+    res.send("Hey!! , I'm just a chatbot web index."); // إرسال رسالة نصية بسيطة كاستجابة للطلب
+});
+
+// بدء تشغيل الخادم والاستماع على المنفذ المحدد
+app.listen(app.get("port"), function(){
+    console.log("Server is running and listening on port " + app.get("port"));
+    // طباعة رسالة في وحدة التحكم توضح أن الخادم يعمل على المنفذ المحدد
+});
