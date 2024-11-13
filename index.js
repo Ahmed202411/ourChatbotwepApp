7abe34204f04a2dad72107de277246f414dffb46
Ahmed202411 <ahmedlabghial@gmail.com>
2024-11-13 18:02:42 +0100

    teste

--- index.js
+++ index.js
@@ -2,48 +2,41 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');  // استيراد مكتبة path
 
-// إنشاء تطبيق Express
 const app = express();
+app.set("port", process.env.PORT || 8000); // إعداد المنفذ
 
-// إعداد المنفذ الذي سيستمع عليه التطبيق
-app.set("port", process.env.PORT || 8000); // استخدام المنفذ من المتغيرات البيئية أو المنفذ 8000 افتراضياً
+// إعدادات body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// إعدادات body-parser لتحليل محتوى الطلبات
-app.use(bodyParser.urlencoded({ extended: false })); // تحليل محتوى البيانات بشكل URL-encoded
-app.use(bodyParser.json()); // تحليل محتوى البيانات بصيغة JSON
-
-// إنشاء مسار GET عند الوصول إلى "/" وإرسال رسالة بسيطة
+// صفحة البداية
 app.get("/", function (req, res) {
-  res.send("Hey!! , I'm just a chatbot web App."); // إرسال رسالة نصية بسيطة كاستجابة للطلب
+  res.send("Hey!! , I'm just a chatbot web App.");
 });
 
-// إضافة دعم لطلبات GET إلى الـ webhook
+// صفحة webhook
 app.get("/webhook", (req, res) => {
   const VERIFY_TOKEN = "VERIFY_TOKEN12345";
-
-  // استخراج المعاملات من الاستعلام
   let token = req.query["hub.verify_token"];
   let challenge = req.query["hub.challenge"];
 
-  // التحقق من التوكن المرسل للتأكد من صحته
   if (token === VERIFY_TOKEN) {
-    console.log("WEBHOOK_VERIFIED");
     res.status(200).send(challenge);
   } else {
-    // إرسال "403 Forbidden" إذا لم تتطابق رموز التحقق
     res.sendStatus(403);
   }
 });
 
+// تقديم ملفات HTML
 app.get('/terms', (req, res) => {
-  res.sendFile(path.join(__dirname, 'terms.html'));  // يجب أن يكون اسم الملف كما هو في النظام
+  res.sendFile(path.join(__dirname, 'terms.html'));  // تأكد من أن الملف موجود في المجلد نفسه
 });
 
 app.get('/privacy', (req, res) => {
-  res.sendFile(path.join(__dirname, 'privacy.html'));  // يجب أن يكون اسم الملف كما هو في النظام
+  res.sendFile(path.join(__dirname, 'privacy.html'));
 });
 
-// بدء تشغيل الخادم والاستماع على المنفذ المحدد
+// بدء الخادم
 app.listen(app.get("port"), function () {
   console.log("Server is running and listening on port " + app.get("port"));
 });
