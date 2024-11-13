40ca9f497f75bb948c9a8e21abc15da3aecb2fdc
Ahmed202411 <ahmedlabghial@gmail.com>
2024-11-13 10:06:21 +0100

    toking

--- index.js
+++ index.js
@@ -17,6 +17,29 @@ app.use(bodyParser.json()); // تحليل محتوى البيانات بصيغة
 app.get("/", function(req, res){
     res.send("Hey!! , I'm just a chatbot web App."); // إرسال رسالة نصية بسيطة كاستجابة للطلب
 });
+// Add support for GET requests to our webhook
+app.get("/webhook", (req, res) => {
+
+        const VEREFY_TOKEN = "VEREFY_TOKEN12345"
+  
+    // Parse the query params
+    //   let mode = req.query["hub.mode"];
+      let token = req.query["hub.verify_token"];
+      let challenge = req.query["hub.challenge"];
+    
+      // Check if a token and mode is in the query string of the request
+    //   if (mode && token) {
+        // Check the mode and token sent is correct
+        if (token === VEREFY_TOKEN) {
+          // Respond with the challenge token from the request
+          console.log("WEBHOOK_VERIFIED");
+          res.status(200).send(challenge);
+        } else {
+          // Respond with '403 Forbidden' if verify tokens do not match
+          res.sendStatus(403);
+        }
+    //   }
+    });
 
 // بدء تشغيل الخادم والاستماع على المنفذ المحدد
 app.listen(app.get("port"), function(){
