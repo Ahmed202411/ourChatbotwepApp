e02f8bc6156b2c72c42e130faebb31eb9203a365
Ahmed202411 <ahmedlabghial@gmail.com>
2024-11-13 12:23:39 +0100

    PolicyTerms

--- index.js
+++ index.js
@@ -14,35 +14,43 @@ app.use(bodyParser.urlencoded({ extended: false })); // تحليل محتوى ا
 app.use(bodyParser.json()); // تحليل محتوى البيانات بصيغة JSON
 
 // إنشاء مسار GET عند الوصول إلى "/" وإرسال رسالة بسيطة
-app.get("/", function(req, res){
-    res.send("Hey!! , I'm just a chatbot web App."); // إرسال رسالة نصية بسيطة كاستجابة للطلب
+app.get("/", function (req, res) {
+  res.send("Hey!! , I'm just a chatbot web App."); // إرسال رسالة نصية بسيطة كاستجابة للطلب
 });
 // Add support for GET requests to our webhook
 app.get("/webhook", (req, res) => {
 
-        const VEREFY_TOKEN = "VEREFY_TOKEN12345"
-  
-    // Parse the query params
-    //   let mode = req.query["hub.mode"];
-      let token = req.query["hub.verify_token"];
-      let challenge = req.query["hub.challenge"];
-    
-      // Check if a token and mode is in the query string of the request
-    //   if (mode && token) {
-        // Check the mode and token sent is correct
-        if (token === VEREFY_TOKEN) {
-          // Respond with the challenge token from the request
-          console.log("WEBHOOK_VERIFIED");
-          res.status(200).send(challenge);
-        } else {
-          // Respond with '403 Forbidden' if verify tokens do not match
-          res.sendStatus(403);
-        }
-    //   }
-    });
+  const VEREFY_TOKEN = "VEREFY_TOKEN12345"
+
+  // Parse the query params
+  //   let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode is in the query string of the request
+  //   if (mode && token) {
+  // Check the mode and token sent is correct
+  if (token === VEREFY_TOKEN) {
+    // Respond with the challenge token from the request
+    console.log("WEBHOOK_VERIFIED");
+    res.status(200).send(challenge);
+  } else {
+    // Respond with '403 Forbidden' if verify tokens do not match
+    res.sendStatus(403);
+  }
+  //   }
+});
+// مسارات لعرض ملفات شروط الخدمة وسياسة الخصوصية
+app.get('/terms', (req, res) => {
+  res.sendFile(path.join(__dirname, 'terms.html'));
+});
+
+app.get('/privacy', (req, res) => {
+  res.sendFile(path.join(__dirname, 'privacy.html'));
+});
 
 // بدء تشغيل الخادم والاستماع على المنفذ المحدد
-app.listen(app.get("port"), function(){
-    console.log("Server is running and listening on port " + app.get("port"));
-    // طباعة رسالة في وحدة التحكم توضح أن الخادم يعمل على المنفذ المحدد
+app.listen(app.get("port"), function () {
+  console.log("Server is running and listening on port " + app.get("port"));
+  // طباعة رسالة في وحدة التحكم توضح أن الخادم يعمل على المنفذ المحدد
 });
